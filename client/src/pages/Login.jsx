422f6b0e50ad1136acc373bcad451a3fed2eb589
kusalwbkd <kusalwbkddhananjaya@gmail.com>
2024-02-05 00:16:13 +0530

    first commit

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Login = () => {
+  return (
+    <>
+      <div>Login</div>
+
+<Link to={'/register'}>Register</Link>
+    </>
+  
+  )
+}
+
+export default Login
---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Register = () => {
+  return (
+    <>
+     <div>Register</div>
+     <Link to={'/login'}>Login</Link>
+    </>
+   
+  )
+}
+
+export default Register
